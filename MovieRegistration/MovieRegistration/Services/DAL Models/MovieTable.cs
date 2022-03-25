85d7136c2e20d3ab226d6f36e131167bb0147e35
Rachel Goode <rgoode92@gmail.com>
2022-03-25 19:55:32 -0400

    initial commit

---
+++ MovieRegistration/MovieRegistration/Services/DAL Models/MovieTable.cs
@@ -0,0 +1,14 @@
+﻿namespace MovieRegistration.Services.DAL_Models
+{
+    public class MovieTable
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+        public string Actor1 { get; set; }
+        public string Actor2 { get; set; }
+        public string Director1 { get; set; }
+        public string Director2 { get; set; }
+    }
+}
---
+++ MovieRegistration/MovieRegistration/Services/MovieContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieRegistration.Services.DAL_Models;
+
+namespace MovieRegistration.Services
+{
+    public class MovieContext : DbContext
+    {
+        public DbSet<MovieTable> movieTables { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+            @"Data Source=localhost;Initial Catalog=movieregistration;Integrated Security=True");
+        }
+    }
+}
